fe56039bb21e7ccd5de2c0316d3f628809f7f112
Seth Stigall <89556479+Sethcbs@users.noreply.github.com>
2023-09-23 04:55:56 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,75 @@
+// Create web server
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', async (req, res) => {
+  const { id } = req.params;
+  const comments = commentsByPostId[id] || [];
+  res.send(comments);
+});
+
+app.post('/posts/:id/comments', async (req, res) => {
+  const { id } = req.params;
+  const { content } = req.body;
+
+  const commentId = randomBytes(4).toString('hex');
+
+  const comments = commentsByPostId[id] || [];
+  comments.push({
+    id: commentId,
+    content,
+    status: 'pending',
+  });
+  commentsByPostId[id] = comments;
+
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: id,
+      status: 'pending',
+    },
+  });
+
+  res.status(201).send(comments);
+});
+
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+  console.log(`Event Received: ${type}`);
+
+  if (type === 'CommentModerated') {
+    const { id, postId, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((c) => c.id === id);
+    comment.status = status;
+
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        postId,
+        status,
+        content,
+      },
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
